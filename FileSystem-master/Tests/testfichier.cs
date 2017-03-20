43ef6c446439955ed4224049aca4d432b04e94ce
Theo Min <theo.minhoto@gmail.com>
2017-03-20 18:44:01 +0100

    InsertionFileSysteme

---
+++ FileSystem-master/Tests/testfichier.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem.Tests
+{
+    [TestClass]
+    public class TestFichier
+    {
+
+        [TestMethod]
+        public void TestCanWriteOK()
+        {
+            Fichier f = new Fichier("Test", null);
+            f.chmod(6);
+
+            Assert.AreEqual(true, f.canWrite());
+        }
+
+        public void TestCanWriteKO()
+        {
+            Fichier f = new Fichier("Test", null);
+            Assert.AreEqual(true, f.canWrite());
+        }
+    }
+}
